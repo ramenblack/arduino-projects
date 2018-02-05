c9019b4965efbfaedeef4badb74ffa73bf297986
Keegan Westphal <31550979+ramenblack@users.noreply.github.com>
2018-02-05 12:27:42 -0800

    Update rx.ino

--- RX-TX/rx.ino
+++ RX-TX/rx.ino
@@ -1,17 +1,15 @@
-int incomingByte = 0;   // for incoming serial data
-
+int incomingByte = 0;   
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+        Serial.begin(9600);     
 }
 
 void loop() {
 
-        // send data only when you receive data:
+        
         if (Serial.available() > 0) {
-                // read the incoming byte:
+      
                 incomingByte = Serial.read();
 
-                // say what you got:
                 Serial.print("I received: ");
                 Serial.println(incomingByte, DEC);
         }
